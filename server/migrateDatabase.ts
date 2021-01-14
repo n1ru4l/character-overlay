f3544a24759cbe6d5e3d51a23594018dbe8e27d3
Laurin Quast <laurinquast@googlemail.com>
2021-01-14 21:48:31 +0100

    feat: run migration inside a transaction

--- server/migrateDatabase.ts
+++ server/migrateDatabase.ts
@@ -7,23 +7,26 @@ export const migrateDatabase = (db: Database) => () => {
   switch (version) {
     case 0: {
       db.exec(/* SQL */ `
-CREATE TABLE IF NOT EXISTS "Character" (
-    "id" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "name" TEXT NOT NULL,
-    "imageUrl" TEXT NOT NULL,
-    "maximumHealth" INTEGER NOT NULL DEFAULT 30,
-    "currentHealth" INTEGER NOT NULL DEFAULT 30,
-    "hasMana" BOOLEAN NOT NULL DEFAULT false,
-    "maximumMana" INTEGER NOT NULL DEFAULT 30,
-    "currentMana" INTEGER NOT NULL DEFAULT 30,
-    "editHash" TEXT NOT NULL,
-    "hasFatePoints" BOOLEAN NOT NULL DEFAULT false,
-    "maximumFatePoints" INTEGER NOT NULL DEFAULT 2,
-    "currentFatePoints" INTEGER NOT NULL DEFAULT 2,
-    PRIMARY KEY ("id")
-);
-CREATE UNIQUE INDEX IF NOT EXISTS "character.editHash_unique" ON "Character"("editHash");
+        BEGIN;
+        PRAGMA "user_version" = 1;
+        CREATE TABLE IF NOT EXISTS "Character" (
+            "id" TEXT NOT NULL,
+            "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            "name" TEXT NOT NULL,
+            "imageUrl" TEXT NOT NULL,
+            "maximumHealth" INTEGER NOT NULL DEFAULT 30,
+            "currentHealth" INTEGER NOT NULL DEFAULT 30,
+            "hasMana" BOOLEAN NOT NULL DEFAULT false,
+            "maximumMana" INTEGER NOT NULL DEFAULT 30,
+            "currentMana" INTEGER NOT NULL DEFAULT 30,
+            "editHash" TEXT NOT NULL,
+            "hasFatePoints" BOOLEAN NOT NULL DEFAULT false,
+            "maximumFatePoints" INTEGER NOT NULL DEFAULT 2,
+            "currentFatePoints" INTEGER NOT NULL DEFAULT 2,
+            PRIMARY KEY ("id")
+        );
+        CREATE UNIQUE INDEX IF NOT EXISTS "character.editHash_unique" ON "Character"("editHash");
+        COMMIT;
      `);
     }
   }
