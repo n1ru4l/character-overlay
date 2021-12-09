64f931dc438e340e87e019770d3de9bed3f0f729
Laurin Quast <laurinquast@googlemail.com>
2021-12-09 16:41:36 +0100

    more code formatting

--- src/gql/fragment-masking.ts
+++ src/gql/fragment-masking.ts
@@ -1,21 +1,17 @@
-import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+import { TypedDocumentNode as DocumentNode } from "@graphql-typed-document-node/core";
 
-
-export type FragmentType<TDocumentType extends DocumentNode<any, any>> = TDocumentType extends DocumentNode<
-  infer TType,
-  any
->
-  ? TType extends { ' $fragmentName': infer TKey }
-    ? TKey extends string
-      ? { ' $fragmentRefs': { [key in TKey]: TType } }
+export type FragmentType<TDocumentType extends DocumentNode<any, any>> =
+  TDocumentType extends DocumentNode<infer TType, any>
+    ? TType extends { " $fragmentName": infer TKey }
+      ? TKey extends string
+        ? { " $fragmentRefs": { [key in TKey]: TType } }
+        : never
       : never
-    : never
-  : never;
-
+    : never;
 
 export function useFragment<TType>(
   _documentNode: DocumentNode<TType, any>,
   fragmentType: FragmentType<DocumentNode<TType, any>>
 ): TType {
-  return fragmentType as any
+  return fragmentType as any;
 }
